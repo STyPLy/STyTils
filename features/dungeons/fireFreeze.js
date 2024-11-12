86f4188e29581e1393f8dbdaf89968867c646791
STyPLy <stuarth.donald@gmail.com>
2024-11-12 02:33:34 -0500

    Recoded auto ff and fixed ghost blocks

--- features/dungeons/fireFreeze.js
+++ features/dungeons/fireFreeze.js
@@ -1,6 +1,29 @@
 import config from '../../config'
 const S02PacketChat = Java.type("net.minecraft.network.play.server.S02PacketChat")
 
+function AutoFreeze() {
+    let inventory = Player.getInventory().getItems()
+    let FF = false
+    let ffIndex
+    inventory.forEach((item,index)=>{
+        if (item?.getName()?.includes("Fire Freeze") && index < 9) {
+            FF = true;
+            ffIndex = index
+        }
+    })
+    if (!FF) return;
+    
+    Player.setHeldItemIndex(ffIndex)
+    if (Player.getHeldItem().getName().includes("Fire Freeze")) {
+        let rc = Client.getMinecraft().class.getDeclaredMethod('func_147121_ag')
+        rc.setAccessible(true)
+        setTimeout(()=>{
+            rc.invoke(Client.getMinecraft())
+        },50)
+    }
+       
+}
+
 register("packetReceived", (packet, event)=>{
     if (packet.func_148916_d()) return
 
@@ -14,12 +37,10 @@ register("packetReceived", (packet, event)=>{
         ChatLib.chat("§4Fire Freeze!")
         Client.showTitle(" ", "§4Fire Freeze!", 0, 30, 0);
         Client.showTitle(" ", "§4Fire Freeze!", 0, 30, 0);
+        World.playSound("mob.ghast.scream",1,1)
+        World.playSound("mob.ghast.scream",1,1)
 
-        if (Player.getHeldItem().getName().includes("Fire Freeze") && config.autoFreeze) {
-            let rc = Client.getMinecraft().class.getDeclaredMethod('func_147121_ag')
-            rc.setAccessible(true)
-            rc.invoke(Client.getMinecraft())
-        }
+        if (config.autoFreeze && Player.getY() == 71) AutoFreeze();
     },2500)
 
 
--- features/dungeons/ghostPick.js
+++ features/dungeons/ghostPick.js
@@ -1,8 +1,10 @@
-const air = new Block(new BlockType("minecraft:air"), new BlockPos(999, 999, 999), null).type.getDefaultState()
-const chest = new Block(new BlockType("minecraft:chest"), new BlockPos(999,999,999),null).type.getDefaultState()
-const lever = new Block(new BlockType("minecraft:lever"), new BlockPos(999,999,999),null).type.getDefaultState()
-const tchest = new Block(new BlockType("minecraft:trapped_chest"), new BlockPos(999,999,999),null).type.getDefaultState()
+const air = new Block(new BlockType("minecraft:air"), new BlockPos(999, 999, 999), null).type
+const chest = new Block(new BlockType("minecraft:chest"), new BlockPos(999,999,999),null).type
+const lever = new Block(new BlockType("minecraft:lever"), new BlockPos(999,999,999),null).type
+const tchest = new Block(new BlockType("minecraft:trapped_chest"), new BlockPos(999,999,999),null).type
 let db = false
+
+let disallowed = [air.getUnlocalizedName(),chest.getUnlocalizedName(),lever.getUnlocalizedName(),tchest.getUnlocalizedName()]
 
 new KeyBind("Ghost Block",Keyboard.KEY_NONE,"STyTils").registerKeyDown(()=>{
     
@@ -13,10 +15,12 @@ new KeyBind("Ghost Block",Keyboard.KEY_NONE,"STyTils").registerKeyDown(()=>{
     },50)
     const mc = Client.getMinecraft()
     let blockpos = Player.getPlayer().func_174822_a(6,1).func_178782_a()
-    let block = Player.getPlayer().field_70170_p.func_180495_p(blockpos)
-    if (block == air || block == chest || block == lever || block == tchest) return
+    
+    
+    let block = Player.getPlayer().field_70170_p.func_180495_p(blockpos).func_177230_c().func_149739_a()
+    if (disallowed.includes(block)) return;
 
     Player.getPlayer().func_71038_i()
-    mc.field_71441_e.func_175656_a(blockpos,air)
+    mc.field_71441_e.func_175656_a(blockpos,air.getDefaultState())
     
 })
