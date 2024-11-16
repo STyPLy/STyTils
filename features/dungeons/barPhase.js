4f67f27387f8711c741a724ec240654e2fcc14f2
STyPLy <stuarth.donald@gmail.com>
2024-11-16 15:25:11 -0500

    Version 1.1.2

--- features/dungeons/barPhase.js
+++ features/dungeons/barPhase.js
@@ -8,7 +8,7 @@ const map = {
 
 const bar = new Block(new BlockType("minecraft:iron_bars"), new BlockPos(123,123,123),null).type.getUnlocalizedName();
 
-
+// Checks if you are able to phase
 function canPhase(num) {
     num = Math.abs(num - Math.floor(num));
     return Math.abs(num - 0.137) <= 5e-4 || Math.abs(num - 0.863) <= 5e-4
@@ -17,14 +17,16 @@ function canPhase(num) {
 register("tick",()=>{
     if (!config.barPhase) return;
     if (!map[Player.facing()]) return;
-
+    
     let pos = [Player.getX(),Player.getY(),Player.getZ()]
+    // Raytraces block in front of you
     let blockpos = Player.getPlayer().func_174822_a(1,1)?.func_178782_a()
-    if (!blockpos) return;
+    if (!blockpos) return
+    
     let block = Player.getPlayer().field_70170_p.func_180495_p(blockpos).func_177230_c().func_149739_a()
     let phase = false
     let newPos = pos.map((value,index) => value + map[Player.facing()][index])
-
+    // checks if raytraced block is a iron bar
     if (block != bar) return
     
     phase = pos.some(canPhase)
