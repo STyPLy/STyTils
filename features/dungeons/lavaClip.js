b78e4857fc059a7ade434535b49fa7cf7daac946
STyPLy <stuarth.donald@gmail.com>
2025-02-23 19:35:39 -0500

    improved lavaclip velocity cancel (again)

--- features/dungeons/lavaClip.js
+++ features/dungeons/lavaClip.js
@@ -1,9 +1,7 @@
 import { getKeybind } from "../../utils";
-const S12PacketEntityVelocity = Java.type("net.minecraft.network.play.server.S12PacketEntityVelocity")
+const S12PacketEntityVelocity = Java.type("net.minecraft.network.play.server.S12PacketEntityVelocity");
 let distance = 28;
 let toggled = false;
-let noVelo = false
-
 
 new KeyBind("Lava Clip", getKeybind("Lava Clip"), "STyTils").registerKeyPress(()=>{
     toggled = !toggled
@@ -18,14 +16,15 @@ register("command",(dist)=>{
 
 register("tick",()=>{
     if (toggled && Player.getPlayer().func_180799_ab()) {
-        toggled = false;
-        Player.getPlayer().func_70107_b(Player.getX(),Player.getY()-distance,Player.getZ())
         let trigger = register("packetReceived", (packet,event) => {
             if (packet?.func_149410_e() != 28000 || packet?.func_149412_c() != Player.getPlayer()?.func_145782_y()) return;
-            cancel(event)
-            trigger.unregister();
-        })
-        Client.scheduleTask(20,()=>{
+            cancel(event);
+        }).setFilteredClass(S12PacketEntityVelocity)
+        
+        toggled = false;
+        Player.getPlayer().func_70107_b(Player.getX(),Player.getY()-distance,Player.getZ())
+        
+        Client.scheduleTask(80,()=>{
             trigger?.unregister();
         })
     }
