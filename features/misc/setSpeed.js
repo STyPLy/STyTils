6675d17563da780c85f40b720bae0ff2a45a0f9f
STyPLy <stuarth.donald@gmail.com>
2025-02-23 22:30:33 -0500

    improved best speed mode

--- features/misc/setSpeed.js
+++ features/misc/setSpeed.js
@@ -4,7 +4,7 @@ register("tick",() => {
     if (!config.setSpeed) return;
     const minecraft = Client.getMinecraft();
     let currentSpeed = minecraft.field_71439_g.field_71075_bZ.func_75094_b()
-    let speed = config.bestSpeed ? currentSpeed + 0.015 : config.speed / 1000
+    let speed = config.bestSpeed ? currentSpeed + (currentSpeed / 33.34) : config.speed / 1000
 
     minecraft.field_71439_g.func_110148_a(Java.type("net.minecraft.entity.SharedMonsterAttributes").field_111263_d).func_111128_a(speed); // minecraft.thePlayer.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(speed)
 })
