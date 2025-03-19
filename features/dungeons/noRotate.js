793e60e96f44015300b021b31ce8fd4433c6a93d
STyPLy <stuarth.donald@gmail.com>
2025-03-19 19:05:03 -0400

    made noRotate more readable

--- features/dungeons/noRotate.js
+++ features/dungeons/noRotate.js
@@ -3,15 +3,27 @@ import config from '../../config';
 const S08PacketPlayerPosLook = Java.type("net.minecraft.network.play.server.S08PacketPlayerPosLook")
 const C03PacketPlayer = Java.type("net.minecraft.network.play.client.C03PacketPlayer")
 
-
 register("packetReceived",(packet,event)=>{
-    if (!config.noRotate) return;
+    if (!config.noRotate || !Player?.getPlayer()) return;
     cancel(event)
-    if (!Player?.getPlayer()) return;
-    let [prevYaw,prevPitch] = [Player?.getPlayer()?.field_70126_B,Player?.getPlayer()?.field_70127_C];
-    let [x,y,z,yaw,pitch] = [packet.func_148932_c(),packet.func_148928_d(),packet.func_148933_e(),packet.func_148931_f(),packet.func_148930_g()];
+
+    let [prevYaw, prevPitch] = [
+        Player.getPlayer()?.field_70126_B,
+        Player.getPlayer()?.field_70127_C
+    ];
     
+    let [x, y, z, yaw, pitch] = [
+        packet.func_148932_c(),
+        packet.func_148928_d(),
+        packet.func_148933_e(),
+        packet.func_148931_f(),
+        packet.func_148930_g()
+    ];
+    
+    // Teleport Player (no rotate)
     Player?.getPlayer()?.func_70107_b(x,y,z)
+    
+    // Send C06 response packet
     Client.sendPacket(new C03PacketPlayer.C06PacketPlayerPosLook(
         Player.getX(),
         Player.getPlayer().func_174813_aQ().field_72338_b,
@@ -20,13 +32,12 @@ register("packetReceived",(packet,event)=>{
         pitch,
         false
     ));
-    if (config.keepMotion) {
-        Player.getPlayer().func_70016_h(Player.getMotionX(),0,Player.getMotionZ());
-    }
-    else {
-        Player.getPlayer().func_70016_h(0,0,0);
-    }
     
+    // ternary operator type shit
+    config.keepMotion 
+        ? Player.getPlayer().func_70016_h(Player.getMotionX(),0,Player.getMotionZ())
+        : Player.getPlayer().func_70016_h(0,0,0);
+    // Update server with new rotation info
     Client.scheduleTask(0,()=>{
         Client.sendPacket(new C03PacketPlayer.C06PacketPlayerPosLook(
             Player.getX(),
